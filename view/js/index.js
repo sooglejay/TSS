b485c79295b7ad0873c2480278e7a1ac8ad9eff9
sooglejay <394055524@qq.com>
2017-12-04 17:36:44 +0800

    增加了 任务管理

--- view/js/index.js
+++ view/js/index.js
@@ -310,30 +310,94 @@ function getTableDataLength() {
     return getTableData().length;
 }
 function getTableData() {
-    return $('#row_1').parent().children();
+    return $('#row_0').parent().children();
+}
+function getRowNumFromIdStr(idStr) {
+    var arr = idStr.split('_');
+    var id = Number(arr[arr.length - 1]);
+    if (!isNaN(id)) {
+        return id;
+    } else {
+        alert('error from getting row number!');
+        return -1;
+    }
+}
+
+function getNewTaskHtml(rowNumber, taskNumber) {
+    var html =
+        '<div class="input-group">' +
+        '<input class="form-control" style="width:90%;align-content: center" id="task_input_' + taskNumber + '_' + rowNumber + '" type="text" value="" placeholder="task name">' +
+        //删除任务
+        '<span onclick="removeTask(this)" class=" glyphicon glyphicon-remove-circle" style="width:1%;margin-top:10px;margin-left:5px;cursor:hand;text-align: center"></span>' +
+        '</div>';
+
+    return html;
+
 }
 
 /**
  * 添加一行
+ * id构造的算法是  ***_行号  也就是说行号是id这个字符串的最后一个数值
  */
 function addRow() {
-    var rowsLen = getTableDataLength();
-    var newRowIndex = rowsLen + 1;
-    var newRow = '<tr id="row_' + newRowIndex + '">' +
-        '<td id="project_' + newRowIndex + '">Project_' + newRowIndex + '</td>' +
-        '<td id="task_' + newRowIndex + '_1">Tasks_' + newRowIndex + '</td>' +
-        '<td id="day_' + newRowIndex + '_1">8</td>' +
-        '<td id="day_' + newRowIndex + '_2">4</td>' +
-        '<td id="day_' + newRowIndex + '_3">5</td>' +
-        '<td id="day_' + newRowIndex + '_4">6</td>' +
-        '<td id="day_' + newRowIndex + '_5">7</td>' +
-        '<td colspan="3" style="min-width: 120px">' +
-        '<button id="btnAddTask_' + newRowIndex + '" class="btn-default">Add Task</button>' +
-        '<button id="btnEdit_' + newRowIndex + '" class="btn-warning pull-left">Edit</button>' +
-        '<button onclick="removeRow(this)" id="btnDelete_' + newRowIndex + '" class="btn-danger pull-right">Delete</button>' +
+    var newRowIndex = getTableDataLength();
+    var newRow =
+        //行开始
+        '<tr id="row_' + newRowIndex + '">' +
+
+        //项目
+        '<td>' +
+        '<input class="form-control" id="project_input_' + newRowIndex + '" type="text" value="" placeholder="project name">' +
         '</td>' +
+
+        //任务  宽带可以设置
+        '<td style="min-width: 150px">' +
+        '<div id="task_wrapper_' + newRowIndex + '" align="center">' +
+        getNewTaskHtml(newRowIndex, 1) +
+        //添加任务
+        '<span onclick="addTask(this)" class="glyphicon glyphicon-play-circle glyphicon-plus-sign" style="margin-top: 5%;cursor: hand"></span>' +
+        '</div>' +
+        '</td>' +
+
+        //周一
+        '<td><input  class="day form-control" type="text" id="day_input_1_' + newRowIndex + '" placeholder="hours"/></td>' +
+        //周二
+        '<td><input  class="day form-control" type="text" id="day_input_2_' + newRowIndex + '" placeholder="hours"/></td>' +
+        //周三
+        '<td><input  class="day form-control" type="text" id="day_input_3_' + newRowIndex + '" placeholder="hours"/></td>' +
+        //周四
+        '<td><input  class="day form-control" type="text" id="day_input_4_' + newRowIndex + '" placeholder="hours"/></td>' +
+        //周五
+        '<td><input  class="day form-control" type="text" id="day_input_5_' + newRowIndex + '" placeholder="hours"/></td>' +
+
+        //操作
+        '<td colspan="2" style="min-width: 120px">' +
+        '<button onclick="editRow(this)" class="btn btn-sm btn-warning pull-left">Edit</button>' +
+        '<button onclick="removeRow(this)" class="btn btn-sm btn-danger pull-right">Delete</button>' +
+        '</td>' +
+
+        //行结束
         '</tr>';
-    $('#row_1').before(newRow);
+    $('#row_0').before(newRow);
+}
+
+/**
+ * 添加一个任务
+ * @param obj
+ */
+function addTask(obj) {
+    var newTaskNumber = obj.parentNode.childNodes.length;
+    var rowNumber = getRowNumFromIdStr(obj.parentNode.id);
+    var taskHtml = getNewTaskHtml(rowNumber, newTaskNumber);
+    $(obj).before(taskHtml);
+}
+
+/**
+ * 删除一个任务
+ * @param obj
+ */
+function removeTask(obj) {
+
 }
 
 /**
@@ -342,55 +406,57 @@ function addRow() {
  */
 function removeRow(obj) {
     var rowsLen = getTableDataLength();
-    var id = obj.id;
+    var id = obj.parentNode.parentNode.id;
     var removeIndex = Number(id.split('_')[1]);
     if (removeIndex == rowsLen) {
         $('#row_' + removeIndex).remove();
     } else {
         $('#row_' + removeIndex).remove();
         var tableData = getTableData();
-        for (var r = removeIndex - 2; r < tableData.length - 1; r++) {
+        for (var r = removeIndex - 1; r < tableData.length - 1; r++) {
             var rowData = tableData[r];
-            var row_id_arr = rowData['id'].split('_');
-            rowData['id'] = row_id_arr[0] + "_" + (Number(row_id_arr[1] - 1));
-
-            //某行的全部数据
-            var childNodes = rowData['childNodes'];
-            for (var cell = 0; cell < childNodes.length; cell++) {
-                // 某行某列的数据
-                var columnData = childNodes[cell];
-                // 列id
-                var col_id = columnData['id'];
-                if (col_id.length < 1) {
-                    var actionCol = columnData['childNodes'];
-                    for (var ac = 0; ac < actionCol.length; ac++) {
-                        var acColumnData = actionCol[ac];
-                        // 列id
-                        col_id = acColumnData['id'];
-                        var a = col_id.split('_');
-                        var nI = (Number(a[1]) - 1);
-                        nI = a[0] + "_" + nI;
-                        acColumnData.id = nI;
-                    }
-                    break;
-                }
-                var arr = col_id.split('_');
-                var newIndex = (Number(arr[1]) - 1);
-                var newId = arr[0] + "_" + newIndex;
-                if (newId >= 1) {// id 型如 **_4_3  4代表行号，3代表列号
-                    newId = newId + "_" + arr[2];
-                }
-                // 删除一行后，下面的行的id要修正
-                childNodes[cell].id = newId;
-
-                // 测试的时候，用这个来看效果。只有projectName和taskName是需要调整的，其他不能改
-                // 注意，正式的时候，下面的代码要删除，或者屏蔽
-                if (cell <= 1) {
-                    childNodes[cell].innerHTML = newId;
-                }
-            }
+            modifyId(rowData);
         }
     }
+}
+
+
+/**
+ * 编辑一行
+ * @param obj
+ */
+function editRow(obj) {
+
+}
+
+/**
+ * 递归修改id算法
+ * 1、删除某行后，其后的各行的id要相应修改
+ * 2、修正id，修改一个元素下的所有子元素的id（如果有的话），使得id减少1
+ * 3、注意，这里的id，是使用'***_***_数字'的格式，'数字'就是行号，行号减1，这就是目的
+ *
+ * @param obj 该元素
+ */
+function modifyId(obj) {
+    var childNodes = obj.childNodes;
+    for (var i = 0; i < childNodes.length; i++) {
+        modifyId(childNodes[i]);
+    }
+    if (obj.id && obj.id.length > 0) {
+        var id = getRowNumFromIdStr(obj.id);
+        if (id > 0) {
+            var arr = obj.id.split('_');
+            id = arr.slice(0, arr.length - 1).join('_') + "_" + (id - 1);
+            obj.id = id;
+        }
+    }
+}
+
+/**
+ * 提交数据 一周的数据，目前，不管如何改动，都会把这一周的数据全部提交
+ */
+function submit() {
+
 }
 
 $(function () {
